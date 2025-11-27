3074b0f7873447563b9f34f3b136e02a169baed2
jfb0301 <bianchi.josefrancisco@gmail.com>
2025-11-27 17:01:27 -0300

    testutil for table-test driven dev

---
+++ pkg/testing/max_test.go
@@ -0,0 +1,32 @@
+// Copyright (c) Efficient Go Authors
+// Licensed under the Apache License 2.0.
+
+package max
+
+import (
+	"math"
+	"testing"
+
+	"github.com/jfb0301/performance_optimization/core/testutil"
+)
+
+// Read more in "Efficient Go"; Example 2-8.
+func TestMax(t *testing.T) {
+	t.Parallel()
+
+	for _, tcase := range []struct {
+		a, b     int
+		expected int
+	}{
+		{a: 0, b: 0, expected: 0},
+		{a: -1, b: 0, expected: 0},
+		{a: 1, b: 0, expected: 1},
+		{a: 0, b: -1, expected: 0},
+		{a: 0, b: 1, expected: 1},
+		{a: math.MinInt64, b: math.MaxInt64, expected: math.MaxInt64},
+	} {
+		t.Run("", func(t *testing.T) {
+			testutil.Equals(t, tcase.expected, max(tcase.a, tcase.b))
+		})
+	}
+}
